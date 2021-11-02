53ca648aca3659dd5e18220fd330dc88d3cee671
Ivan Alles <ivan.alles@gmail.com>
2021-11-02 11:53:41 +0100

    Install modules.

--- install.bat
+++ install.bat
@@ -4,3 +4,6 @@ pipenv install
 :: The dependencies shall be placed into Pipfile.
 
 pipenv run pip install --no-dependencies torchvision==0.11.1+cu102 -f https://download.pytorch.org/whl/cu102/torch_stable.html
+
+pipenv run pip install --no-dependencies face-alignment
+pipenv run pip install --no-dependencies numba
